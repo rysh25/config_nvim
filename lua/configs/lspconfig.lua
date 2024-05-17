014e41073e794b4c475cc47a82d227e3fc648a18
Ryohei Shiraki <rysh25@gmail.com>
2024-05-18 08:44:09 +0900

    Improve lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,21 +1,63 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local configs = require("nvchad.configs.lspconfig")
+
+local on_init = configs.on_init
+local capabilities = configs.capabilities
+
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- 保存時に自動フォーマット
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = { "*.rs", "*.py", "*.ts", "*.go", "*.c", "*.h", "*.cpp", "*.hpp" },
+      callback = function()
+        vim.lsp.buf.format({
+          buffer = ev.buf,
+          timeout_ms = 200,
+          filter = function(f_client)
+            -- TypeScriptのようにNode.js, Deno, Bunのどれを使うかによって、
+            -- none-ls (Biome, Prettier) の有無が変わる場合、none-ls の複数回実行を防止するため
+            return f_client.name ~= "null-ls"
+          end,
+          async = false,
+        })
+      end,
+    })
+  end,
+})
+
+-- Set updatetime for CursorHold
+-- 300ms of no cursor movement to trigger CursorHold
+vim.opt.updatetime = 100
+
+-- Show diagnostic popup on cursor hover
+local diag_float_grp = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
+  group = diag_float_grp,
+})
+
+local function set_keymap(client, buffer)
+  local keymap_opts = { buffer = buffer }
+
+  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
+
+  -- Goto previous/next diagnostic warning/error
+  vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, keymap_opts)
+  vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
+end
+
+local function on_attach(client, buffer)
+  require("nvchad.configs.lspconfig").on_attach(client, buffer)
+
+  set_keymap(client, buffer)
+end
 
 local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
-local servers = { "html", "cssls", "gopls", "clangd" }
-
-lspconfig.rust_analyzer.setup({
-  settings = {
-    ["rust-analyzer"] = {
-      check = {
-        command = "clippy",
-      },
-    },
-  },
-})
+local servers = { "html", "cssls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -25,6 +67,21 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- rust-analyzer
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
+  settings = {
+    ["rust-analyzer"] = {
+      check = {
+        command = "clippy",
+      },
+    },
+  },
+})
 
 -- typescript
 lspconfig.tsserver.setup {
@@ -40,7 +97,7 @@ lspconfig.gopls.setup {
   capabilities = capabilities,
   cmd = {"gopls"},
   filetypes = {"go", "gomod", "gowork", "gotmpl"},
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
   settings = {
     gopls = {
       completeUnimported = true,
@@ -51,7 +108,6 @@ lspconfig.gopls.setup {
     }
   },
 }
-
 
 -- clangd
 lspconfig.clangd.setup {
--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -4,7 +4,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.formatting.gofmt,
-    null_ls.builtins.formatting.clang_format,
+--    null_ls.builtins.formatting.clang_format,
   },
 })
 
